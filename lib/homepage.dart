00802c3c1ab2b98fc7f0c64ac5dcbf63925926d6
Jana18kh <jana12288@gmail.com>
2024-12-01 02:04:50 +0300

    k

---
+++ lib/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:ghasham/main.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+
+        ),
+        ),
+    );
+  }
+}
