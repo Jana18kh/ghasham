280f3f4d7695d9dff161e3941e56e323ad671c58
Jana18kh <jana12288@gmail.com>
2024-12-01 04:22:51 +0300

    d

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const Homepage());
@@ -10,29 +15,98 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text("أهلًا جنى")),
-      body: Container(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      color: Colors.amber,
-                      height: 50,
-                      width: 50,
-                    )
-                  ],
-                )
-              ],
-            )
-          ],
-        ),
-      )
-      
-    ));
+        home: Directionality(
+            textDirection: TextDirection.rtl,
+            child: Scaffold(
+                appBar: AppBar(
+                  title: const Text("أهلًا جنى"),
+                  toolbarHeight: 60,
+                  shadowColor: Color.fromARGB(255, 0, 0, 0),
+                  elevation:0.9,
+                  backgroundColor: Color(0xffF5F5F5),
+                ),
+                body: Container(
+                  color: Color(0xffF5F5F5),
+                    padding: EdgeInsets.only(left: 30, right: 30, top: 20),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: Color(0xffD9D9D9),
+                                      borderRadius: BorderRadius.circular(20)),
+                                  height: 220,
+                                  width: 330,
+                                  margin: EdgeInsets.only(left: 30),
+                                ),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      color: Color(0xffD9D9D9),
+                                      borderRadius: BorderRadius.circular(20)),
+                                  height: 220,
+                                  width: 330,
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.centerRight,
+                            margin:
+                                EdgeInsets.only(top: 40, bottom: 20, right: 10),
+                            child: Text(
+                              'المواد:',
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Color(0xffD9D9D9),
+                                    borderRadius: BorderRadius.circular(20)),
+                                height: 130,
+                                width: 155,
+                                margin: EdgeInsets.only(left: 20),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Color(0xffD9D9D9),
+                                    borderRadius: BorderRadius.circular(20)),
+                                height: 130,
+                                width: 155,
+                              ),
+                            ],
+                          ),
+                          Container(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Color(0xffD9D9D9),
+                                    borderRadius: BorderRadius.circular(20)),
+                                height: 130,
+                                width: 155,
+                                margin: EdgeInsets.only(left: 20),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                    color: Color(0xffD9D9D9),
+                                    borderRadius: BorderRadius.circular(20)),
+                                height: 130,
+                                width: 155,
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )))));
   }
 }
